a61040e92f345f2db41c7bbb87678de09bbdd829
Baklay Petro <pustota1988@gmail.com>
2023-06-04 19:07:38 +0300

    createExpressValidation

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+import { registerValidation } from './validations.js';
 
 const app = express();
 
 app.use(express.json());
 
 mongoose
-    .connect('mongodb+srv://admin:admin@cluster0.ielym5z.mongodb.net/current-bank?retryWrites=true&w=majority')
+    .connect(
+        'mongodb+srv://admin:admin@cluster0.ielym5z.mongodb.net/current-bank?retryWrites=true&w=majority',
+    )
     .then(() => console.log('DB OK'))
     .catch((err) => console.log('BD error', err));
 
@@ -15,8 +19,15 @@ app.get('/', (req, res) => {
     res.send('hello');
 });
 
-app.post('/auth/register', (req, res) => {
-   
+app.post('/auth/register', registerValidation, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array())
+    }
+
+    res.json({
+        success: true,
+    })
 });
 
 app.listen(4444, (err) => {
