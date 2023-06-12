77f31cf7b1a16f2fe56e976b6e481e0197ca437a
Baklay Petro <pustota1988@gmail.com>
2023-06-12 20:46:22 +0300

    addMulter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 import { bankCreateValidation, loginValidation, registerValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 
@@ -7,6 +8,18 @@ import { register, login, getMe } from './controllers/UserController.js';
 import * as BankController from './controllers/BankController.js';
 
 const app = express();
+app.use('/uploads', express.static('uploads'));
+
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
 
 app.use(express.json());
 
@@ -20,6 +33,12 @@ mongoose
 app.get('/', (req, res) => {
     res.send('hello');
 });
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+      url: `/uploads/${req.file.originalname}`,
+    });
+  });
 
 app.post('/auth/login', loginValidation, login);
 app.post('/auth/register', registerValidation, register);
